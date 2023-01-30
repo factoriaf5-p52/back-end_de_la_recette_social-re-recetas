e5f6578c70bf01a1cfc8deb1af78e6f263d5353a
mori <armigofire@gmail.com>
2023-01-30 16:06:35 +0100

    deleted duped files

--- src/ingredients/interfaces/ingredient.interface.ts
+++
@@ -1,6 +0,0 @@
-export interface Ingredient {
-    _id: string;
-    name: string;
-    quantity: number;
-    measure_unit: string;
-}
--- src/ingredients/interfaces/ingredients.interface.ts
+++
@@ -1,6 +0,0 @@
-export interface IngredientItf {
-    _id: string;
-    name: string;
-    measure_unit: string;
-    quantity: number;
-}
