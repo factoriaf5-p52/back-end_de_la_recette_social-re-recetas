3102c116f422e06cda95e757cb74ed7a93476856
gbixio <galbixio93@gmail.com>
2023-01-31 16:10:38 +0100

    testing findAll recipes.controller

--- src/recipes/schemas/recipe.schema.ts
+++ src/recipes/schemas/recipe.schema.ts
@@ -20,8 +20,8 @@ export class Recipe {
   @Prop()
   description: string;
 
-  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: "User" })
-  author: ObjectId;
+  // @Prop({ type: mongoose.Schema.Types.ObjectId, ref: "User" })
+  // author: ObjectId;
 
   @Prop([{ type: mongoose.Schema.Types.ObjectId, ref: "Ingredient" }])
   ingredients: Array<ObjectId>;
@@ -40,9 +40,6 @@ export class Recipe {
 
   @Prop()
   views: number;
-
-  @Prop([String]) 
-  keywords: string[];
 
   @Prop([CommentSchema]) 
   comments: Comment[];
